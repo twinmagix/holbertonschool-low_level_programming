a1dc31b78df126ec7601cdc406be8d72294aa385
twinmagix <Sazindwandwe1997@gmail.com>
2022-02-04 12:09:28 +0200

    New commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	
+        /* print lowercase letters */
+
+	char ch;
+
+        for (ch= ‘a’; ch<= ‘z’; ch++){
+
+                  putchar(ch);
+
+                  putchar (‘\n’) ;
+
+}
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	
+        /* print lowercase letters except q and e */
+
+	char ch;
+
+        for (ch= ‘a’; ch<= ‘z’; ch++)
+	    {
+	      if (ch != 'q' && ch != 'e')
+		 {
+                  putchar(ch);
+		 }
+                  putchar (‘\n’) ;
+
+
+	return (0);
+}
+
