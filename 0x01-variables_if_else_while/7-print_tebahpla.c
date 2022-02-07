81de5edde29a3f2cbf3810c2bae554a3514872e0
twinmagix <Sazindwandwe1997@gmail.com>
2022-02-07 09:50:44 +0200

    New commit

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -7,11 +7,9 @@
 */
 int main(void)
 {
-	int i;
-	for (i = 122; i > 96; i--)
-	{
-	putchar(i);
-	}
-	putchar('\n');
-   return (0);
+int i;
+for (i = 122; i > 96; i--)
+putchar(i);
+putchar('\n');
+return (0);
 }
