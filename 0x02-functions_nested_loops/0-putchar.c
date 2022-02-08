997ba6ade6f4b377eb99f8d7236f0ce1aaef7f71
twinmagix <Sazindwandwe1997@gmail.com>
2022-02-08 10:26:53 +0200

    New commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - represent Entry Point to the program
+ *
+ * Return: 0 to stop the program
+ */
+int main()
+{
+  
+    // Get the character to be written
+    char ch = 'G';
+  
+    // Write the Character to stdout
+    putchar(ch);
+  
+    return (0);
+}
