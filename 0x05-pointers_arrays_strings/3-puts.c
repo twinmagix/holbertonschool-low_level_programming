2c2b5eb1ea3f2a1ed0addcd258db3bff9ab46eca
twinmagix <Sazindwandwe1997@gmail.com>
2022-02-14 12:33:18 +0200

    New commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio>
 /**
  *_puts - prints a string, to stdout
  *@str: value to be evaluate.
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,19 +1,24 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * print_rev - toma la cadena de la funcion.
- * @s: value taken from main
- * Return: (0)
- */
+  * print_rev - Prints a string in reverse
+  * @s: The string to print
+  *
+  * Return: void
+  */
 void print_rev(char *s)
 {
-	int len;
+	int c = 0;
 
-	for (len = 0; s[len] != '\0'; len++)
+	while (s[c] != '\0')
 	{
+		c++;
 	}
-	for (len = len - 1; len >= 0; len--)
+
+	for (c -= 1; c >= 0; c--)
 	{
-		_putchar(s[len]);
+		_putchar(s[c]);
 	}
+
 	_putchar('\n');
 }
