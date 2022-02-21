e3b90cf208e92e126a6af4cb4550e1222daa9ac7
twinmagix <Sazindwandwe1997@gmail.com>
2022-02-21 14:34:57 +0200

    commit

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,34 +1,31 @@
-#include <stnlib.h>
 #include "main.h"
 /**
-* _strstr - locates a substring
-*
-* @haystack: the longer string to search
-* @needle: the substring to search for
-*
-* Return: a pointer to the beginning of the located substring, or NULL if
-* the substring is not found.
-*/
-
+ * _strstr - Locate a substring
+ * @haystack: String to locate
+ * @needle: Substring to locate
+ * Return: A string at starting or null
+ */
 char *_strstr(char *haystack, char *needle)
 {
 	int i;
-	int s = 0;
+	int j;
 
-	while (needle[s] != '\0')
-		s++;
+	if (needle[0] == '\0')
+		return (haystack);
 
-	while (*haystack)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (i = 0; needle[i]; i++)
+		if (haystack[i] == needle[0])
 		{
-			if (haystack[i] != needle[i])
-				break;
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] != needle[j])
+					break;
+			}
+			if (needle[j] == '\0')
+				return (haystack + i);
 		}
-		if (i != s)
-			haystack++;
-		else
-			return (haystack);
+
 	}
-	return (NULL);
+	return ('\0');
 }
--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,27 +1,20 @@
 #include "main.h"
 /**
- * print_chessboard - prints the chessboard
- *
- * @a: the row of the array
- *
- * Return: nothing
+ * print_chessboard - Print chessboard
+ * @a: Print
+ * Return: A crap
  */
-
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
+	int i;
+	int j;
 
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
-			if (j == 7)
-			{
-				_putchar(a[i][j]);
-				_putchar('\n');
-			}
-			else
-				_putchar(a[i][j]);
+			_putchar(a[i][j]);
 		}
+		_putchar('\n');
 	}
 }
