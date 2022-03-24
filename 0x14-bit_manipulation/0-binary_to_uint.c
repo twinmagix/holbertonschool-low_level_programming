c569ef8ce4450f01bef868a1b9d659ad1f24cc6f
twinmagix <Sazindwandwe1997@gmail.com>
2022-03-24 15:30:43 +0200

    first commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,36 +1,33 @@
-/*
- * Auth: Sazi A. Ndwandwe
- * File: 0-binary_to_uint.c
- */
-
 #include "main.h"
-
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: A pointer to a string of 0 and 1 chars.
- *
- * Return: If b is NULL or contains chars not 0 or 1 - 0.
- *         Otherwise - the converted number.
- */
+ *binary_to_uint - convert binary to integer
+*@b:binary number
+*Return: number in decimal base
+*/
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, mult = 1;
-	int len;
+	unsigned int k = 1;
+	unsigned int i = 0;
+	int c;
+	unsigned int len;
 
-	if (b == '\0')
+	if (!b)
 		return (0);
-
-	for (len = 0; b[len];)
-		len++;
-
-	for (len -= 1; len >= 0; len--)
+	while (b[i] != '\0')
 	{
-		if (b[len] != '0' && b[len] != '1')
-			return (0);
-
-		num += (b[len] - '0') * mult;
-		mult *= 2;
+		i++;
 	}
-
-	return (num);
-}
+	len = i;
+	i = 0;
+	for (c = len - 1; c >= 0; c--)
+	{
+		if (b[c] != '0' && b[c] != '1')
+			return (0);
+		if (b[c] == '1')
+		{
+			i += k;
+		}
+		k *= 2;
+	}
+	return (i);
+	}
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,10 +1,8 @@
 /*
- * File: 1-print_binary.c
  * Auth: Sazi A. Ndwandwe
  */
 
 #include "main.h"
-
 /**
  * print_binary - Prints the binary representation of a number.
  * @n: The number to be printed in binary.
